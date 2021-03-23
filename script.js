7ae817d1e73831ca17ed7e0a6abf852690c077ab
Mekan <mekanmukhammedov@gmail.com>
2021-03-23 12:45:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// 'use strict';
+a = 5;
+console.log(a);
+// let leftBorderWidth = 1;{
+//     let second = 2;
+// }
+
+// const pi=3.14;
+// console.log(second);
+
+let arr= ['types.jpeg'];
+console.log(arr[0]);
+
+// // alert("Hello World")
+// let answer = confirm('Are you here')
+
+// let answer =+prompt('Are you 18 years old?', 'Yes')
+// console.log(typeof(answer));
+// console.log('arr' +  ' -  object');
+// console.log(4 + '- object');
+
+
+let incr = 10,
+    decr = 10;
+    
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    
+    console.log('2' === 2);
+
+    let isChecked= true,
+        isClose = false;
+        console.log(isChecked || !isClose);
